c1fe1a4672de09e89e3598a2dc45ba07bfabbbaa
Никита Митрофанов <133259451+Egoisted@users.noreply.github.com>
2023-05-11 21:26:21 +0300

    Create Laba8_1var.pas
    Лаба 8

---
+++ Laba8_1var.pas
@@ -0,0 +1,20 @@
+﻿program Laboratornaya8;
+type 
+x = set of 'а'..'я';
+var glas,sogl,txt: x;
+k:char;
+n,m: integer;
+begin
+ n:=0;
+ m:=0;
+ glas:=['й','у','е','ы','а','о','э','я','и','ю'];
+ sogl:=['ц','к','н','г','ш','щ','з','х','ъ','ф','в','п','р','л','д','ж','ч','с','м','т','ь','б'];
+ txt:=['п','р','и','в','е','т'];
+ for  k:='а' to 'я' do begin
+ if k in txt then
+ if k in sogl then m:=m+1 else if k in glas then n:=n+1;
+ end;
+ writeln('Гласных = ',n,' Согласных = ',m);
+ if n>m then writeln('Гласных больше чем солгасных') else writeln('Согласных больше чем гласных');
+ if n=m then writeln('Гласных и согласных равное количество');
+end.
